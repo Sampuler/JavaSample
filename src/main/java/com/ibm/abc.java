373c45b136b7680effd6932f6ebd734a951cbc37
Sampuler <bairiswaraj17@gmail.com>
2023-07-31 12:22:08 +0530

    intial commit

---
+++ src/main/java/com/ibm/abc.java
@@ -0,0 +1,16 @@
+package com.ibm;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+   @Controller
+    public class abc {
+	@GetMapping("/welcome")
+	public String getWelcome(Model model) {
+	model.addAttribute("msg", "welcome to kirru");
+	
+    return "index";
+
+	}
+}
