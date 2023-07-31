90bd36221720f1d20dc6a99863abe5dd6649df33
Sampuler <bairiswaraj17@gmail.com>
2023-07-31 12:26:05 +0530

    updated

--- src/main/java/com/ibm/abc.java
+++ src/main/java/com/ibm/abc.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 	@GetMapping("/welcome")
 	public String getWelcome(Model model) {
 	model.addAttribute("msg", "welcome to kirru");
+	System.out.println("hello world");
 	
     return "index";
 
